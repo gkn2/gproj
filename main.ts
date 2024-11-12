e87b9aae577045395a33ae79f0ddfae53efcff07
gkn2 <112503427+gkn2@users.noreply.github.com>
2024-11-12 11:13:31 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,58 @@
+function SpawnRoadCars () {
+    roadcars = [sprites.create(img`
+        . . . . . . e e c c e e . . . . 
+        . . . . . e 2 2 2 2 2 2 e . . . 
+        . . . . 2 c 2 2 2 2 2 2 c 2 . . 
+        . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+        . . . f 2 2 4 2 2 2 2 2 c 2 f . 
+        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+        . . . f 2 c 2 4 4 2 2 2 c 2 f . 
+        . . . e 2 c e c c c c e c 2 e . 
+        . . . e 2 e c b b b b c e 2 e . 
+        . . . e 2 e b b b b b b e 2 e . 
+        . . . e e e e e e e e e e e e . 
+        . . . f e d e e e e e e d e f . 
+        . . . f e 2 d e e e e d 2 e f . 
+        . . . f f e e e e e e e e f f . 
+        . . . . f f . . . . . . f f . . 
+        `, SpriteKind.Enemy), sprites.create(img`
+        . . . . . . 8 8 c c 8 8 . . . . 
+        . . . . . 8 6 6 6 6 6 6 8 . . . 
+        . . . . 6 c 6 6 6 6 6 6 c 6 . . 
+        . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
+        . . . f 6 6 9 6 6 6 6 6 c 6 f . 
+        . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+        . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+        . . . f 6 c 6 9 9 6 6 6 c 6 f . 
+        . . . 8 6 c 8 c c c c 8 c 6 8 . 
+        . . . 8 6 8 c b b b b c 8 6 8 . 
+        . . . 8 6 8 b b b b b b 8 6 8 . 
+        . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . . f 8 d 8 8 8 8 8 8 d 8 f . 
+        . . . f 8 6 d 8 8 8 8 d 6 8 f . 
+        . . . f f 8 8 8 8 8 8 8 8 f f . 
+        . . . . f f . . . . . . f f . . 
+        `, SpriteKind.Enemy), sprites.create(img`
+        . . . . . . a a c c a a . . . . 
+        . . . . . a 3 3 3 3 3 3 a . . . 
+        . . . . 3 c 3 3 3 3 3 3 c 3 . . 
+        . . . a 3 c d 3 3 3 3 3 c 3 a . 
+        . . . f 3 3 d 3 3 3 3 3 c 3 f . 
+        . . . f 3 3 d 3 3 3 3 3 3 3 f . 
+        . . . f 3 3 d 3 3 3 3 3 3 3 f . 
+        . . . f 3 c 3 d d 3 3 3 c 3 f . 
+        . . . a 3 c a c c c c a c 3 a . 
+        . . . a 3 a c b b b b c a 3 a . 
+        . . . a 3 a b b b b b b a 3 a . 
+        . . . a a a a a a a a a a a a . 
+        . . . f a d a a a a a a d a f . 
+        . . . f a 3 d a a a a d 3 a f . 
+        . . . f f a a a a a a a a f f . 
+        . . . . f f . . . . . . f f . . 
+        `, SpriteKind.Enemy)]
+}
+let roadcars: Sprite = null
 let mySprite = sprites.create(img`
     . . . . . . . . . . b 5 b . . . 
     . . . . . . . . . b 5 b . . . . 
@@ -16,7 +71,11 @@ let mySprite = sprites.create(img`
     b b c c c d d d d 5 5 5 b b . . 
     . . . c c c c c c c c b b . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite, 180, 360)
-scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite, 100, 0)
 scene.setBackgroundImage(assets.image`myImage`)
 scroller.scrollBackgroundWithCamera(scroller.CameraScrollMode.OnlyVertical)
+scroller.scrollBackgroundWithSpeed(0, 80)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    roadcars.setPosition(randint(35, 135), randint(0, 60))
+})
